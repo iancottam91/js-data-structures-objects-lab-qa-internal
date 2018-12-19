25b0b0acf2546857c313f419cdd5fef0d41507d7
Ian Cottam <ian.cottam@bbc.co.uk>
2018-12-19 16:50:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+let driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  return {
+    ...driver,
+    [key] : value,
+  }
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  let newDriver = {
+    ...driver
+  }
+  
+  delete newDriver[key];
+  
+  return newDriver;
+}
